20ccdc0b5ae56e9c7a9b7e78d5639e001f6e25da
yansj1001 <2990285987@qq.com>
2023-05-15 10:51:26 +0800

    初始化准备

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,10 +1,15 @@
-import './assets/main.css'
+import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+//测试接口函数
+import { getCategory } from './apis/testAPI'
+getCategory().then(res=>{
+    console.log(res);
+})
 
 const app = createApp(App)
 
--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -1,14 +1,42 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+//配置element-plus自动按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    //配置element-plus自动按需导入
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      //resolvers: [ElementPlusResolver()],
+      resolvers: [
+        //配置element-plus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: "sass" })
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+          @use "@/styles/var.scss" as *;
+        `,
+      }
     }
   }
 })
